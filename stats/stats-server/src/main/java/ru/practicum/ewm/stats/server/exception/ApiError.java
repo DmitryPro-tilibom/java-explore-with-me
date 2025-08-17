7e9bb27fd4395d99b84622f339a7038a22db9702
Tilibom1983 <ygra@list.ru>
2025-08-17 17:51:46 +0300

    fix stat exception

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/exception/ApiError.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/server/exception/ApiError.java
@@ -1,2 +1,20 @@
-package ru.practicum.ewm.stats.server.exception;public class ApiError {
+package ru.practicum.ewm.stats.server.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.ewm.stats.dto.EndpointHitDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+public class ApiError {
+    private final List<String> errors;
+    private final String message;
+    private final String reason;
+    private final String status;
+    @JsonFormat(pattern = EndpointHitDto.DATE_TIME_PATTERN)
+    private final LocalDateTime timestamp;
 }
